29fdcc59409fd20408efb06ab74ff84da8241835
rkoutia <84357017+rkoutia@users.noreply.github.com>
2021-05-20 23:56:28 -0700

    First Release

--- navbar.js
+++ navbar.js
@@ -14,10 +14,13 @@ const navBarHtml = `
                 <i class="fa fa-caret-down"></i>
             </button>
             <div class="dropdown-content">
-                <a href="./background/scope.html">Scope</a>
+                <a href="./background/scope.html">Retail Industry Scope</a>
                 <a href="./background/market.html">Market Overview</a>
                 <a href="./background/technology.html">Technology Overview</a>
-            </div>
+                <a href="./background/globaltrends.html">Global Threat Trends</a>
+                <a href="./background/retailtrends.html">Retail Threat Trends</a>
+                <a href="./background/threatactors.html">Threat Actors</a>
+                </div>
         </div>
 
         <div class="dropdown">
@@ -25,10 +28,8 @@ const navBarHtml = `
                 <i class="fa fa-caret-down"></i>
             </button>
             <div class="dropdown-content">
-                <a href="./threatTrends/globaltrends.html">Global Threat Trends</a>
-                <a href="./threatTrends/threatactors.html">Threat Actors</a>
-                <a href="./threatTrends/threatstats.html">Threat Trend Stats</a>
-                <a href="./threatTrends/criticalassetid.html">Critical Asset Identification</a>
+                <a href="./threatTrends/criticalassets.html">Critical Assets</a>
+                <a href="./threatTrends/intelligencebuyin.html">Intelligence Buyin</a>
             </div>
         </div>
         
@@ -37,17 +38,41 @@ const navBarHtml = `
                 <i class="fa fa-caret-down"></i>
             </button>
             <div class="dropdown-content">
-                <a href="./diamondModels/loginData.html">Customer Website Login Data</a>
-                <a href="./diamondModels/hr.html">HR Information</a>
+                <a href="./diamondModels/logindata.html">Website Login Data</a>
+                <a href="./diamondModels/customerdata.html">Customer Data</a>
+                <a href="./diamondModels/employeedata.html">Employee Data</a>
+                <a href="./diamondModels/employeecredentials.html">Employee Credentials</a>
+                <a href="./diamondModels/proprietaryinfo.html">Proprietary Info</a>
+                <a href="./diamondModels/supplierdata.html">Supplier Data</a>
+                <a href="./diamondModels/internaldata.html">Internal Data</a>
+                <a href="./diamondModels/hrinfo.html">HR Info and Workflows</a>
+                <a href="./diamondModels/supplychaindisruption.html">Supply Chain Disruption</a>
+                <a href="./diamondModels/firewallcompromise.html">Firewall Compromise</a>
+                <a href="./diamondModels/networkdisruption.html">Network Disruption</a>
+                <a href="./diamondModels/posskimmer.html">POS Skimmer</a>
+                <a href="./diamondModels/barcodereader.html">Barcode Reader</a>
+                <a href="./diamondModels/crosssitescripting.html">Cross Site Scripting</a>
+                <a href="./diamondModels/ransomware.html">Ransomware Attack</a>
+                <a href="./diamondModels/unpatchedserver.html">Unpatched Server</a>
+                <a href="./diamondModels/ddosattack.html">DDOS Attack</a>
             </div>
         </div>
+
+        <div class="dropdown">
+        <button class="dropbtn">References
+            <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-content">
+            <a href="./references/ctiprocess.html">CTI Process Info</a>
+            <a href="./references/references.html">Document References</a>
+        </div>
+    </div>
         <!-- 
         <a href="insights.html">Key Insights</a>
         <a href="operationalintel.html">Operational Intel</a>
         <a href="platform.html">Platform Direction</a>
         --> 
         <a href="aboutus.html">About Us</a>
-        <a href="references.html">References</a>
     </div>
 
 </html> 
